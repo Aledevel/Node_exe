09476ee0e859abd17985bede5404ff9c84ef34de
AleDevel <alessiod651@gmail.com>
2024-03-19 16:13:02 +0100

    mission completed

--- esercizio_4.mjs
+++ esercizio_4.mjs
@@ -20,3 +20,5 @@ server.listen(3000, () => {
 
 // Change the location in the response to "Mars". Run the server and make a request to it with curl using the --verbose flag.
 //  What is the value of the Content-Length response header?
+
+//Content-Length: 19
