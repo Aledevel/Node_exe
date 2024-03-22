1f2ce4afab5d71932c56ece6666ef8bfaedd7f85
AleDevel <alessiod651@gmail.com>
2024-03-22 09:57:53 +0100

    esercizio 7 completato

--- esercizio_7.mjs
+++ esercizio_7.mjs
@@ -1,18 +1,36 @@
-Create agetResults function that uses async and await. Inside of the function, call the luckyDraw function for each of the players: Tina, Jorge, Julien
-
-Log out the resolved value for each promise and handle any promise rejections.
+// Create agetResults function that uses async and await. Inside of the function,
+// call the luckyDraw function for each of the players: Tina, Jorge, Julien
+// Log out the resolved value for each promise and handle any promise rejections.
 
 function luckyDraw(player) {
   return new Promise((resolve, reject) => {
     const win = Boolean(Math.round(Math.random()));
 
     process.nextTick(() => {
-      if (win) {
-        resolve(`${player} won a prize in the draw!`);
+      try {
+        if (win) {
+        resolve(console.log(`${player} won a prize in the draw!`));
       } else {
-        reject(new Error(`${player} lost the draw.`));
+        throw new error;
       }
+      } catch (error) {
+       console.log(`${player} lost the draw.`) ;
+        
+      }
+      
     });
   });
 }
+let player_1 = "Tina";
+let player_z = "Jorge";
+let player_xz = "Julien";
 
+function Player(player,callback) {
+  callback(player);
+}
+
+Player(player_1, luckyDraw);
+
+Player(player_z, luckyDraw);
+
+Player(player_xz, luckyDraw);
