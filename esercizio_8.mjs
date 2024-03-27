fb81a8b536337a345c9ab504408a0e2f24637496
AleDevel <alessiod651@gmail.com>
2024-03-27 12:09:46 +0100

    esercizio 8

--- esercizio_8.mjs
+++ esercizio_8.mjs
@@ -4,26 +4,40 @@
 
 
 
-const { EventEmitter } = require("node:events");
+import { error } from "node:console";
+import { EventEmitter,  } from "node:events";
+const emitter = new EventEmitter();
+
+
 
 function createNewsFeed() {
-  const emitter = new EventEmitter();
 
   setInterval(() => {
-    emitter.emit("newsEvent", "News: A thing happened in a place.");
+    emitter.emit("newsEvent","News: A thing happened in a place.");
   }, 1000);
 
   setInterval(() => {
-    emitter.emit("breakingNews", "Breaking news! A BIG thing happened.");
+    emitter.emit("breakingNews","Breaking news! A BIG thing happened.");
   }, 4000);
 
   setTimeout(() => {
-    emitter.emit("error", new Error("News feed connection error"));
+    emitter.emit("error",new Error("News feed connection error"));
   }, 5000);
 
   return emitter;
 }
 
+
 const newsFeed = createNewsFeed();
 
 
+
+newsFeed.on("newsEvent",(data)=>{
+  console.log(data)
+})
+newsFeed.on("breakingNews",(data)=>{
+  console.log(data)
+})
+newsFeed.on("error",(error)=>{
+  console.error(error)
+})
